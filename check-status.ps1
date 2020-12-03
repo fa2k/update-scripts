9ecd22be790a60ada9a0e90b15e0360c040a76a9
fa2k <pmb@fa2k.net>
2020-12-03 12:47:03 +0100

    Check the update installation status

--- check-status.ps1
+++ check-status.ps1
@@ -12,6 +12,8 @@ $PatchGroupMembers =      (Get-ADGroupMember -Identity PatchGroup0),
 
 $NoPatchComputers = Get-ADGroupMember -Identity NoPatchGroup
 
+
+
 # Checking that all computers are in one of these groups
 $KnownComputersSids = ($PatchGroupMembers | ForEach-Object { $_ }) + $NoPatchComputers | ForEach-Object { $_.SID }
 $UnknownComputers = $AllComputers | Where-Object SID -NotIn $KnownComputersSids
@@ -22,6 +24,23 @@ if ($UnknownComputers)
     Write-Error "This is an error. Please fix and then re-run the script."
 }
 
+Write-Host "Getting new cumulative updates from updatelist file..."
+if (Test-Path D:\updatelist.txt) {
+    Get-Content -Path D:\updatelist.txt | `
+                    ForEach-Object { Get-WsusUpdate -UpdateId $_ } | `
+                    Where-Object { $_.Update.Title -match "^\d\d\d\d-\d\d Cumulative " } | `
+                    Tee-Object -Variable NewCumUpdates
+}
+else {
+    $NewCumUpdates = @()
+}
+if (-not $NewCumUpdates) {
+    Write-Host "No updates matching the criteria were found. 'Installed' counts will be zero."
+}
+
+$TargetGroups = (Get-WsusServer).GetComputerTargetGroups()
+
+
 foreach ($group in $PatchGroups) {
     Write-Host ""
 	Write-Host "**** Patch group $group status *****"
@@ -31,14 +50,28 @@ foreach ($group in $PatchGroups) {
 
     $AdComputers = $PatchGroupMembers[$group] | Get-ADComputer
     $WsusGroupMembers = Get-WsusComputer -ComputerTargetGroups "PatchGroup$group-SecOnly","PatchGroup$group-All"
-    
+    $ThisTargetGroups = $TargetGroups | Where-Object { $_.Name -like "PatchGroup$group-*" }
+    $CumlUpdateStatuses = $NewCumUpdates | `
+                            ForEach-Object {
+                                $update = $_
+                                $ThisTargetGroups | ForEach-Object {
+                                            $update.Update.GetUpdateInstallationInfoPerComputerTarget($_)
+                                            
+                                            }
+                                }
+
     foreach ($computer in $AdComputers) {
-        $Result = $Computer | Select Name,WsusLastReport,LastBackup
+        $Result = $Computer | Select Name,WsusLastReport,CumlInstCount,LastBackup
 
         # Get WSUS status
         $WsusComp = $WsusGroupMembers | Where-Object { $_.FullDomainName -like $Computer.Name -or $_.FullDomainName -like "$($computer.Name).nscamg.local"}
-        if (($WsusComp | measure).Count -eq 1) {
+        if ($WsusComp.count -eq 1) {
             $Result.WsusLastReport = $WsusComp.LastReportedStatusTime
+            $CumlPatchesInstalled = $CumlUpdateStatuses | `
+                                        Where-Object { $_.ComputerTargetId -eq $WsusComp.Id -and `
+                                                       $_.UpdateInstallationState -eq "Installed" }
+            $Result.CumlInstCount = $CumlPatchesInstalled.count
+                                        
         }
         else {
             Write-Warning "Computer '$($computer.Name)' is not in any WSUS target for patch group $group."
